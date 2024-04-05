6c2e7c6881da3d638efc71fce310804ca723f1b3
Raphael Matta <125845104+R-Matta-1@users.noreply.github.com>
2024-04-05 11:51:49 -0400

    canvas

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// get vis, get DOMParser, learn fetch()
+const canvas = document.getElementById('canvas')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
